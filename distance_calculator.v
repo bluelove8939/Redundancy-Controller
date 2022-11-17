408f6d7779fe48f3ffa8fc49788166fb368ea91f
bluelove8939 <bluelove8939@naver.com>
2022-11-17 17:50:31 +0900

    Initial commit

--- distance_calculator.v
+++ distance_calculator.v
@@ -7,14 +7,19 @@ module DistanceCalculator #(
     parameter DIST_WIDTH    = 7,
     parameter MAX_LIFM_RSIZ = 4
 ) (
-    input [WORD_WIDTH-1:0] idx1,
-    input [WORD_WIDTH-1:0] idx2,
+    input [WORD_WIDTH-1:0] idx1,  // smaller index of redundant operand pair
+    input [WORD_WIDTH-1:0] idx2,  // larger index of redundant operand pair
 
+    input [WORD_WIDTH-1:0] ld,  // lowered distance
     input [WORD_WIDTH-1:0] ow,  // output width
-    input [WORD_WIDTH-1:0] kw,  // kernel width
+    input [WORD_WIDTH-1:0] fw,  // filter(kernel) width
     input [WORD_WIDTH-1:0] st,  // stride amount
 
-    output []
+    output valid,  // indicates whether the value is valid
+
+    output [DIST_WIDTH-1:0] dr,
 );
+
+// Stage: Calculating dv = (i2 % FW) - (i1 % FW)
     
 endmodule
